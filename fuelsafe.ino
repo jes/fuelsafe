cfdd14f3033967b4a4b1056fb6b232022b7253ec
James Stanley <james@incoherency.co.uk>
2020-09-23 14:09:50 +0100

    Add first guess at Arduino sketch

---
+++ fuelsafe.ino
@@ -0,0 +1,18 @@
+const int analogInPin = A3; // pin for photoresistor
+const int analogOutPin = 0; // pin on programming header to copy photoresistor reading
+const int digitalOutPin = 4; // pin for output signal
+
+const int threshold = 500;
+
+void setup() {
+  pinMode(analogInPin, INPUT);
+  pinMode(analogOutPin, OUTPUT);
+  pinMode(digitalOutPin, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(analogInPin);
+  analogWrite(analogOutPin, sensorValue / 4);
+  digitalWrite(digitalOutPin, sensorValue > threshold);
+  delay(2);
+}
